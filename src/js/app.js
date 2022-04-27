94f3d709187da5b139156f14ec75ff533f66ed82
arm-band <bre4tn.let@gmail.com>
2022-04-27 23:41:32 +0900

    - ハンバーガーアイコン仮置き

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ import Ukiyo from 'ukiyojs';
 window.addEventListener( 'load', () => {
     const ukiyoParallaxes = document.querySelectorAll( '.ukiyo' );
     ukiyoParallaxes.forEach( (ukiyoParallax) => {
-        new Ukiyo( ukiyoParallax );
+        new Ukiyo( ukiyoParallax, {
+            scale: 1.75,
+            speed: 1.5,
+        } );
     });
 });
