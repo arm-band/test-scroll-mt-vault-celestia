70cd23c9aeca2a31ba89e54cacb5f43671788f93
arm-band <bre4tn.let@gmail.com>
2022-04-28 22:43:28 +0900

    ブレークポイント未満のナビゲーションメニューでアンカーリンク移動時に自動的にメニューを閉じる機能を追加。プレーンJSで置換。

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,71 @@
 import Ukiyo from 'ukiyojs';
 
-window.addEventListener( 'load', () => {
-    const ukiyoParallaxes = document.querySelectorAll( '.ukiyo' );
-    ukiyoParallaxes.forEach( (ukiyoParallax) => {
+const hamburgerClose = (e) => {
+    const navbar = document.querySelector('#navbar');
+    const navBarListID = 'navbarList';
+    if (
+        typeof e.currentTarget.closest(`#${navBarListID}`) !== 'undefined'
+         && e.currentTarget.closest(`#${navBarListID}`) !== null
+    ) {
+        let breakpoint = 0;
+        if (/(^|\s)navbar-expand-(\S*)/g.test(navbar.className)) {
+            switch (RegExp.$2) {
+                case 'sm':
+                    breakpoint = 576;
+                    break;
+                case 'md':
+                    breakpoint = 768;
+                    break;
+                case 'lg':
+                    breakpoint = 992;
+                    break;
+                case 'xl':
+                    breakpoint = 1200;
+                    break;
+                case 'xxl':
+                    breakpoint = 1400;
+                    break;
+                default:
+                    breakpoint = 0;
+                    break;
+            }
+        }
+        if (window.innerWidth < breakpoint) {
+            // ブレークポイント未満の幅のとき
+            const navbarTogglers = navbar.querySelectorAll(`.navbar-toggler[data-bs-target="#${navBarListID}"]`);
+            navbarTogglers.forEach((navbarToggler) => {
+                if(!navbarToggler.classList.contains('collapsed')) {
+                    // 現在の表示がハンバーガーメニューの場合、
+                    // 移動したらハンバーガーを折りたたむ
+                    navbarToggler.dispatchEvent(new Event('click'));
+                }
+                else if(
+                    e.currentTarget.classList.contains('dropdown-item')
+                     && e.currentTarget.closest('.dropdown').classList.contains('show')
+                ) {
+                    // 現在の表示がハンバーガーメニューではなく、ドロップダウン内のメニューをクリックした場合
+                    // 移動したらドロップダウンを折りたたむ
+                    e.currentTarget.closest('.dropdown').dispatchEvent(new Event('click'));
+                }
+            });
+        }
+    }
+};
+
+window.addEventListener('load', () => {
+    // ブランド名とドロップダウンコンポーネント 「以外」 のナビゲーションリスト
+    const navLinks = document.querySelectorAll('.navbar-brand, .nav-item:not(.dropdown) a, .dropdown-item');
+    navLinks.forEach((navLink) => {
+        // 第二引数に hamburgerClose(navLink) のように関数を渡すと、関数の実行結果が渡されてしまうのでNG。名前だけにする。逆に e でイベントターゲットは拾えるので引数を与える必要はない
+        navLink.addEventListener('click', hamburgerClose, false);
+    });
+
+    const ukiyoParallaxes = document.querySelectorAll('.ukiyo');
+    ukiyoParallaxes.forEach((ukiyoParallax) => {
         new Ukiyo( ukiyoParallax, {
             scale: 1.75,
-            speed: 1.5,
-        } );
+            speed: 1.75,
+        });
     });
-});
+},
+false);
